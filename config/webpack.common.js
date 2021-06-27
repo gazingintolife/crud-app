a98a85d9c48fdb8a71115274fc25c65352d0dadd
unknown <avinash.rathee1@gmail.com>
2021-06-26 22:40:57 -0400

    webpack.common.js updated

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -3,7 +3,6 @@ const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const helpers = require('./helpers');
 
