7219cee2b9984ee8abf29ad20a17bc172a8c2533
unknown <avinash.rathee1@gmail.com>
2021-06-27 19:59:55 -0400

    Config File Deleted

--- config/mongo.config.example.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    db: `mongodb+srv://userIsAvinash:Avi-nash1@cluster0.eedjs.mongodb.net/myFirstDatabase?authSource=admin&retryWrites=true&w=majority`,
-    db_dev: 'mongodb://127.0.0.1:27017/database',
-  };
-
-  console.log("from config",process.env.DB_USER,process.env.DB_PASS)
