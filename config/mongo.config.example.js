9d3dbee7ca1188f097306b292f4abee9af5ff927
unknown <avinash.rathee1@gmail.com>
2021-06-27 19:37:47 -0400

    bugs fixed

--- config/mongo.config.example.js
+++ config/mongo.config.example.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    db: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eedjs.mongodb.net/myFirstDatabase?authSource=admin&retryWrites=true&w=majority`,
+    db: `mongodb+srv://userIsAvinash:Avi-nash1@cluster0.eedjs.mongodb.net/myFirstDatabase?authSource=admin&retryWrites=true&w=majority`,
     db_dev: 'mongodb://127.0.0.1:27017/database',
   };
 
