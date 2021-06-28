0ed560d58ec5a214bca7d284c19bedaa72754751
unknown <avinash.rathee1@gmail.com>
2021-06-28 00:44:50 -0400

    bugs fixed

--- config/mongo.config.example.js
+++ config/mongo.config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db : `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eedjs.mongodb.net/myFirstDatabase?authsource=admin&retryWrites=true&w=majority`
+    db: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.eedjs.mongodb.net:27017,cluster0-shard-00-01.eedjs.mongodb.net:27017,cluster0-shard-00-02.eedjs.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-yakf1q-shard-0&authSource=admin&retryWrites=true&w=majority`
 }
 
 console.log(process.env.DB_USER, process.env.DB_PASS);
